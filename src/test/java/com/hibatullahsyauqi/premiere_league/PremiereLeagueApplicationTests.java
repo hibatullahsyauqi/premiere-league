4cee0225fe669dfbda9cd3763ae4fd0bc4b4c2d4
Hibatullah Syauqi At Taqi <hibatullah_syauqi@hotmail.com>
2025-06-17 21:35:37 +0700

    first commit

---
+++ src/test/java/com/hibatullahsyauqi/premiere_league/PremiereLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibatullahsyauqi.premiere_league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiereLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
